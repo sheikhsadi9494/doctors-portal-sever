9b5cab2c5776dd96c54d32583900a55b45756de4
sheikhsadi9494 <sadis6343@gmail.com>
2022-02-02 19:58:01 +0600

    process .env added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-const serviceAccount = require('./doctors-portal-firebase-adminsdk.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
